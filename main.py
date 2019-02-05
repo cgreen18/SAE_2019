d1fe4cb9271023b4de14ee17e0c569d073c5ac45
cgreen18 <35274689+cgreen18@users.noreply.github.com>
2019-02-04 20:12:34 -0800

    Updated main.py
    Compiles but doesn't work. Reduced number of methods

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ csvtog = False
 global csvfile
 csvfile = open('SAE_Data.csv','wb')
 
+thiswriter = csv.writer(csvfile, delimiter = ' ', quoting=csv.QUOTE_MINIMAL)
+thiswriter.writerow(['Time:             ' , 'Ground speed:        ', 'Roll:', 'Pitch:', 'Altitude:'])
 
 #dataArr:
 #row1 - time
@@ -49,32 +51,6 @@ window.geometry("1540x840") #You want the size of the app to be 500x500
 back = tk.Frame(window,bg='black')
 window.configure(background='black')
 
-
-
-
-
-# get flight data
-def getFlightData():
-    groundSpeed = vehicle.groundspeed
-    roll = vehicle.attitude.roll
-    pitch = vehicle.attitude.pitch
-    altitude = vehicle.location.global_relative_frame.alt
-    if altitude < 0:    # Dont let the dropTime become imaginary
-        altitude = 0
-
-    print 'run'
-
-    global csvtog
-
-    if csvtog:
-        print 'running'
-        timeNow = time.strftime('%y-%m-%d %H:%M:%S')
-        global csvfile
-        thiswriter = csv.writer(csvfile, delimiter = ' ', quoting=csv.QUOTE_MINIMAL)
-        thiswriter.writerow([timeNow , groundSpeed, roll, pitch, altitude])
-
-    return (groundSpeed, roll, pitch, altitude)
-
 helv46 = tkFont.Font(family='Verdana', size=120)
 helv = tkFont.Font(family='Verdana', size= 55)
 data_x = 1010
@@ -86,6 +62,7 @@ label_y = 100
 
 verd24 = tkFont.Font(family='Verdana', size=24)
 
+
 # gets the altitude information
 alt_label = Label(text = "Altitude (ft)", font = verd24, bg = 'black', fg = 'white').place(x=label_x,y=label_y)
 alt1 = ''
@@ -93,16 +70,7 @@ telem = Label(window, font = helv46, bg = 'black', fg = 'yellow')
 telem.pack(fill= BOTH, expand = 1)
 telem.place(x=data_x-60, y=label_y+40)
 
-def getAlt():
-    global alt1
-    (groundSpeed, roll, pitch, altitude) = getFlightData()
-    altitude = int(altitude*3.28084)
-    if altitude != alt1:
-        al1 = altitude
-        telem.config(text = altitude)
-    telem.after(100,getAlt)
-    
-getAlt()
+
 
 # gets the grounding speed information
 speed_label = Label(text = "Speed (ft/s)", font = verd24 ,bg = 'black', fg = 'white').place(x = label_x+300, y = label_y)
@@ -110,19 +78,6 @@ speed1 = ''
 speed_text = Label(window, font = helv46, bg = 'black', fg = 'orange')
 speed_text.pack(fill= BOTH, expand = 1)
 speed_text.place(x=data_x+300-40, y=label_y+40)
-
-def getSpeed():
-    global speed1
-    (groundSpeed, roll, pitch, altitude) = getFlightData()
-    groundSpeed = int(groundSpeed*3.28084)
-    if groundSpeed != speed1:
-        speed1 = groundSpeed
-        speed_text.config(text = groundSpeed)
-    # calls itself every 100 milliseconds
-    # to update the speed display as needed
-    speed_text.after(100,getSpeed)
-getSpeed()
-
 
 
 # get latitude
@@ -132,16 +87,6 @@ lat_info = Label(window, font = helv, bg = 'black', fg = 'lime green')
 lat_info.pack(fill= BOTH, expand = 1)
 lat_info.place(x= data_x-150, y=label_y+180+65+60)
 
-def getLat():
-    global lat1
-    lat2 = round(vehicle.location.global_frame.lat, 3)
-    if lat2 != lat1:
-        lat1 = lat2
-        lat_info.config(text = lat2)
-    # calls itself every 100 milliseconds
-    # to update the latitude display as needed
-    lat_info.after(100,getLat)
-getLat()
 
 # get longitude
 long_label = Label(text = " Longitude", font = verd24, bg = 'black', fg = 'white').place(x=label_x + 300, y=label_y+180+60)
@@ -150,45 +95,28 @@ long_info = Label(window, font = helv, bg = 'black', fg = 'red2')
 long_info.pack(fill= BOTH, expand = 1)
 long_info.place(x=data_x+120, y=label_y+180+65+60)
 
-def getLong():
-    global long1
-    long2 = round(vehicle.location.global_frame.lon, 3)
-    if long2 != long1:
-        long1 = long2
-        long_info.config(text = long2)
-    # calls itself every 100 milliseconds
-    # to update the speed display as needed
-    long_info.after(200,getLong)
-getLong()
-
-# get yaw information
-yaw_label = Label(text = "Yaw (deg)", font = verd24, bg = 'black', fg = 'white').place(x=label_x, y = label_y+360+85)
-yaw1 = ''
-yaw_info = Label(window, font = helv, bg = 'black', fg = 'cyan2')
-yaw_info.pack(fill= BOTH, expand = 1)
-yaw_info.place(x=data_x-110, y=label_y+360+100+55)
-
-def getYaw():
-    global yaw1
-    yaw2 = round(vehicle.attitude.yaw*57.2958, 2)
-    if yaw2 != yaw1:
-        yaw1 = yaw2
-        yaw_info.config(text = yaw2)
-    # calls itself every 100 milliseconds
-    # to update the speed display as needed
-    yaw_info.after(100,getYaw)
-getYaw()
-
-#this creates label texts for altitude, the date, and speed
 
 # make a time stamp
+global time1
 time1 = ''
+global clock
 clock = Label(window, font=('Verdana', 26), bg='black', fg = 'white')
 clock.pack(fill=BOTH, expand=1)
 clock.place(x=1100,y=20)
 
+
+running = tk.Text(background = "green")
+running.place(x=200,y=300)
+
+#Main loop, calls every 
+#
+#
+#
+
 def tick():
+    print 'tick'
     global time1
+    global clock
     # get the current local time from the PC
     time2 = time.strftime('%y-%m-%d %H:%M:%S')
     # if time string has changed, update it
@@ -197,8 +125,60 @@ def tick():
         clock.config(text=time2)
     # calls itself every 200 milliseconds
     # to update the time display as needed
-    clock.after(200, tick)
-tick()
+    clock.after(200,getFlightData)
+    
+
+
+# get flight data
+def getFlightData():
+    groundSpeed = vehicle.groundspeed
+    roll = vehicle.attitude.roll
+    pitch = vehicle.attitude.pitch
+    altitude = vehicle.location.global_relative_frame.alt
+    if altitude < 0:    # Dont let the dropTime become imaginary
+        altitude = 0
+
+    global csvtog
+
+    if csvtog:
+        timeNow = time.strftime('%y-%m-%d %H:%M:%S')
+        global csvfile
+        thiswriter = csv.writer(csvfile, delimiter = ' ', quoting=csv.QUOTE_MINIMAL)
+        thiswriter.writerow([timeNow , groundSpeed, roll, pitch, altitude])
+
+    updateHUD(groundSpeed, roll, pitch, altitude)
+
+    return (groundSpeed, roll, pitch, altitude)
+
+
+
+
+
+def updateHUD(groundSpeed, roll, pitch, altitude):
+    altitude = int(altitude*3.28084)
+    
+    if altitude != alt1:
+        al1 = altitude
+        telem.config(text = altitude)
+
+    groundSpeed = int(groundSpeed*3.28084)
+
+    if groundSpeed != speed1:
+        speed1 = groundSpeed
+        speed_text.config(text = groundSpeed)
+
+    lat2 = round(vehicle.location.global_frame.lat, 3)
+    if lat2 != lat1:
+        lat1 = lat2
+        lat_info.config(text = lat2)
+
+    long2 = round(vehicle.location.global_frame.lon, 3)
+    if long2 != long1:
+        long1 = long2
+        long_info.config(text = long2)
+
+    return
+    
 
 # create the functions that display which payload was dropped
 def CDA():    
@@ -215,11 +195,12 @@ def habitat():
 
 def toggleCSV():
     global csvtog
-    print csvtog 
     csvtog = not csvtog
     if csvtog:
         #global CSV_button
-        CSV_Button.config(text = 'Stop Logging')
+        CSV_button.config(text = 'Stop Logging')
+    else:
+        CSV_button.config(text = 'Log Data')
     return
 
 def quitcommand():
@@ -240,9 +221,11 @@ btn_x = 30
 btn_y = 740
 
 # create buttons for dropping the payloads
-CDA_button = Button(window, text = "CDA", command = CDA, font = helv36, height = 2, width = 12, fg = "white", borderwidth = 0, bg = 'grey30').place(x = btn_x, y = btn_y)
+CDA_button = tk.Button(window, text = "CDA", command = CDA, font = helv36, height = 2, width = 12, fg = "white", borderwidth = 0, bg = 'grey30').place(x = btn_x, y = btn_y)
 #global CSV_button
-CSV_button = Button(window, text = "Log Data", command = toggleCSV, font = helv36, height = 2, width = 12, fg = "white", borderwidth = 0, bg = 'grey30').place(x = btn_x, y = btn_y-175)
+CSV_button = tk.Button(window, text = "Log Data", command = toggleCSV, font = helv36, height = 2, width = 12, fg = "white", borderwidth = 0, bg = 'grey30')
+CSV_button.place(x = btn_x, y = btn_y-175)
+
 supply_button = Button(window, text = "Supplies", command = supply, font = helv36, height = 2, width = 12, fg = "white", borderwidth = 0, bg = 'grey30').place(x = btn_x + 175, y = btn_y)
 habitat_button = Button(window, text = "Habitat", command = habitat, font = helv36, height = 2, width = 12, fg = "white", borderwidth = 0, bg = 'grey30').place(x = btn_x + 175+175, y = btn_y)
 stop = Button(window, text = "Quit", command = quitcommand, font = helv36, height = 2, width = 12, fg = "red", borderwidth = 0, bg = 'grey30').place(x = btn_x+3*175, y = btn_y)
